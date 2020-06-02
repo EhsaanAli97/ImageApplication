37790b42452736cf85a27580606c7bbca3f37340
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2020-06-02 11:32:27 +0100

    Finalised Image Application

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	state = {
+		term: '',
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		this.props.onSubmit(this.state.term);
+	};
+
+	render() {
+		return (
+			<div className="ui segment">
+				<form className="ui form" onSubmit={this.handleSubmit}>
+					<div className="field">
+						<label>Search Images</label>
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={(e) =>
+								this.setState({
+									term: e.target.value,
+								})
+							}
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
